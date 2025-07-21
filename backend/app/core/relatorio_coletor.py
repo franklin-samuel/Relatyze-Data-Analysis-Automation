ffb100c52b3128435a865c2f783dfdfbf6643126
Pedrohenrc <pirriquecsousa@gmail.com>
2025-07-21 14:38:53 -0300

    Add TikTok report to general report collection

--- backend/app/core/relatorio_coletor.py
+++ backend/app/core/relatorio_coletor.py
@@ -1,15 +1,18 @@
 from app.services.instagram_service import obter_relatorio_semanal_instagram
 from app.services.facebook_service import obter_relatorio_semanal_facebook
 from app.services.youtube_service import obter_relatorio_semanal_youtube
+from app.services.tiktok_service import obter_relatorio_semanal_tiktok
 
 def coletar_relatorio_geral():
     instagram = obter_relatorio_semanal_instagram()
     facebook = obter_relatorio_semanal_facebook()
     youtube = obter_relatorio_semanal_youtube()
+    tiktok = obter_relatorio_semanal_tiktok("saopaulofc")
 
     return {
         "instagram": instagram,
         "facebook": facebook,
-        "youtube": youtube
+        "youtube": youtube,
+        "tiktok": tiktok
     }
 
